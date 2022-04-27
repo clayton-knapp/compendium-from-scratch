999701fff3951f6faedd735ea9fc69ebef8cf372
Clayton Knapp <csjknapp@gmail.com>
2022-04-26 20:02:53 -0500

    got pokemon displaying working on search input

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import styles from '../App.css';
 
-export default function Item() {
-  return <div>Item</div>;
+export default function Item({ pokemon }) {
+  return (
+    <div className={styles['item']} style={{ backgroundColor: pokemon.color_1 }}>
+      <h2>{pokemon.pokemon}</h2>
+      <h3>Type: {pokemon.type_1}</h3>
+      <img src={pokemon.url_image} alt="pokemon" />
+    </div>
+  );
 }
--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,5 +1,60 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import Item from '../components/Item';
+import styles from '../App.css';
 
 export default function List() {
-  return <div>List</div>;
+  // set state
+  const [pokemonList, setPokemonList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState('');
+  const [filteredPokemon, setFilteredPokemon] = useState([]);
+
+  const isSearching = !!search.length;
+  const noResults = isSearching && !filteredPokemon.length;
+  const list = isSearching ? filteredPokemon : pokemonList;
+
+  function handleSearch(e) {
+    setSearch(e.target.value);
+    const results = pokemonList.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(e.target.value.toLowerCase().trim())
+    );
+    setFilteredPokemon(results);
+    console.log('filteredPokemon', filteredPokemon);
+  }
+
+  // useEffect
+  useEffect(() => {
+    async function getAndSetPokemon() {
+      const resp = await fetch(
+        'https://pokedex-alchemy.herokuapp.com/api/pokedex'
+      );
+      const data = await resp.json();
+      // console.log('data', data.results);
+      setPokemonList(data.results);
+      setLoading(false);
+    }
+    getAndSetPokemon();
+  }, []);
+
+  // console.log('pokemonList', pokemonList);
+
+  return (
+    <>
+      <input
+        placholder="Find a Pokemon"
+        value={search}
+        onChange={handleSearch}
+      ></input>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <div className={styles.list}>
+          {list.map((pokemon, i) => (
+            <Item key={pokemon.pokemon + i} pokemon={pokemon} />
+          ))}
+        </div>
+      )}
+    </>
+  );
 }
